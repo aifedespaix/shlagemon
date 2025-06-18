92fe2097aabe2d62439f1c8f7bfa7e3a196de547
Aife <clapette01@gmail.com>
2025-06-18 13:48:35 +0200

    Add Schlagedex component

--- src/app/layout/game/game.ts
+++ src/app/layout/game/game.ts
@@ -2,15 +2,17 @@ import { Component } from '@angular/core';
 import { Card } from '../card/card';
 import { GameStateService } from '../../core/game-state.service';
 import { ChoiceDialog } from '../../features/shlagemon/choice-dialog/choice-dialog';
+import { Schlagedex } from '../../features/shlagemon/schlagedex/schlagedex';
+import { SchlagedexService } from '../../features/shlagemon/schlagedex.service';
 
 @Component({
   selector: 'app-game',
-  imports: [ChoiceDialog],
+  imports: [ChoiceDialog, Schlagedex],
   templateUrl: './game.html',
   styleUrl: './game.scss'
 })
 export class Game {
 
-  constructor(private gameState: GameStateService) { }
+  constructor(private gameState: GameStateService, public dex: SchlagedexService) { }
 
 }
