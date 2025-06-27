fb179bbd898f015019e1cba5c7240e99a08ae599
Aife <clapette01@gmail.com>
2025-06-27 11:17:32 +0200

    Fix GameGrid mobile-first layout

--- src/components/layout/GameGrid.vue
+++ src/components/layout/GameGrid.vue
@@ -4,16 +4,16 @@
 
 <template>
   <div
-    class="game grid grid-cols-12 grid-rows-12 h-full gap-2"
-    md="flex flex-col h-auto"
+    class="game h-auto flex flex-col"
+    md="grid grid-cols-12 grid-rows-12 h-full gap-2"
   >
-    <div class="zone left col-span-3 row-span-12" md="col-span-12 row-span-1" />
-    <div class="zone right col-span-3 row-span-12 col-start-10" md="col-span-12 row-span-1" />
-    <div class="zone top col-span-6 row-span-1 col-start-4" md="col-span-12 row-span-1" />
-    <div class="zone center col-span-6 row-span-9 col-start-4 row-start-2" md="col-span-12 row-span-1">
+    <div class="zone left col-span-12 row-span-1" md="col-span-3 row-span-12" />
+    <div class="zone right col-span-12 row-span-1" md="col-span-3 row-span-12 col-start-10" />
+    <div class="zone top col-span-12 row-span-1" md="col-span-6 row-span-1 col-start-4" />
+    <div class="zone center col-span-12 row-span-1" md="col-span-6 row-span-9 col-start-4 row-start-2">
       <slot />
     </div>
-    <div class="zone bottom col-span-6 row-span-2 col-start-4 row-start-11" md="col-span-12 row-span-1" />
+    <div class="zone bottom col-span-12 row-span-1" md="col-span-6 row-span-2 col-start-4 row-start-11" />
   </div>
 </template>
 
