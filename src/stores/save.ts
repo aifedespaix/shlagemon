587c8853d90d7732437fde022cdcd5d27fca8c98
Aife <clapette01@gmail.com>
2025-06-29 17:44:36 +0200

    reset stores when clearing save

--- src/stores/save.ts
+++ src/stores/save.ts
@@ -1,14 +1,17 @@
 import { defineStore } from 'pinia'
+import { useGameStore } from './game'
 import { useGameStateStore } from './gameState'
 import { useShlagedexStore } from './shlagedex'
 
 export const useSaveStore = defineStore('save', () => {
   const dex = useShlagedexStore()
   const gameState = useGameStateStore()
+  const game = useGameStore()
 
   function reset() {
-    dex.clear()
+    dex.reset()
     gameState.reset()
+    game.reset()
   }
 
   return { reset }
