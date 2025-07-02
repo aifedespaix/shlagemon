bbe8ab9116f8f3cccef3e99cb8fd9f1577921704
Aife Portable <clapette01@gmail.com>
2025-07-02 09:23:57 +0200

    toastify theme logic

--- src/modules/toastify.ts
+++ src/modules/toastify.ts
@@ -4,7 +4,9 @@ import type { UserModule } from '~/types'
 import Vue3Toastify from 'vue3-toastify'
 
 export const install: UserModule = ({ app }) => {
+  const isDark = useDark()
   app.use(Vue3Toastify, {
     autoClose: 5000,
+    theme: isDark.value ? 'dark' : 'light',
   } as ToastContainerOptions)
 }
