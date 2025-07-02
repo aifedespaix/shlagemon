1ff0d349679dbdb6ef7f5425c4943d9f906ad82b
Aife Portable <clapette01@gmail.com>
2025-07-02 13:05:42 +0200

    shlagemon : qulbudrogue

--- src/data/shlagemons.ts
+++ src/data/shlagemons.ts
@@ -136,6 +136,15 @@ export const abraquemar: BaseShlagemon = {
   coefficient: 1,
 }
 
+export const qulbudrogue: BaseShlagemon = {
+  id: 'qulbudrogue',
+  name: 'Qulbudrogué',
+  color: '#d4b05c',
+  description: `Qulbudrogué est connu pour son attitude détendue et sa démarche nonchalante. Il donne l’impression d’être dans un état de réflexion constante… ou complètement à l’ouest. Il se balade en sifflotant des sons qu’il est probablement le seul à comprendre. On raconte qu’il voit la vie en "slow motion" et qu’il communique avec les autres Pokémon par télépathie — mais uniquement quand il en a envie, c’est-à-dire rarement.`,
+  types: [shlagemonTypes.psy],
+  coefficient: 3,
+}
+
 export const starters: BaseShlagemon[] = [carapouffe, salamiches, bulgrosboule]
 export const allShlagemons: BaseShlagemon[] = [
   carapouffe,
@@ -152,4 +161,5 @@ export const allShlagemons: BaseShlagemon[] = [
   goubite,
   alaouakbar,
   abraquemar,
+  qulbudrogue,
 ]
