31e216b246477005f331cb7dbf5b6658336a1320
Aife <clapette01@gmail.com>
2025-07-03 15:35:07 +0200

    feat: add item details modal

--- src/data/items/shlageball.ts
+++ src/data/items/shlageball.ts
@@ -5,6 +5,7 @@ export const balls: Ball[] = [
     id: 'shlageball',
     name: 'Shlagéball',
     description: 'Permet de capturer des Shlagémons sauvages.',
+    details: 'Permet de capturer le Shlagémon actuellement en combat. Moins il a de points de vie, plus la chance de capture augmente.',
     price: 10,
     image: '/items/shlageball/shlageball.png',
     catchBonus: 1,
--- src/type/item.ts
+++ src/type/item.ts
@@ -2,6 +2,11 @@ export interface Item {
   id: string
   name: string
   description: string
+  /**
+   * Detailed explanation of how the item works.
+   * When not provided, `description` will be used instead.
+   */
+  details?: string
   price: number
   image?: string
 }
