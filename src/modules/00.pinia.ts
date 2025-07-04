b1673a7c18bf3e5af15c5e98caedcc764e0dec0a
Aife <clapette01@gmail.com>
2025-07-04 15:30:32 +0200

    fix: persist audio settings

--- src/modules/00.pinia.ts
+++ src/modules/00.pinia.ts
@@ -7,7 +7,8 @@ import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 // https://pinia.vuejs.org/
 export const install: UserModule = ({ isClient, initialState, app }) => {
   const pinia = createPinia()
-  pinia.use(piniaPluginPersistedstate)
+  if (isClient)
+    pinia.use(piniaPluginPersistedstate)
   app.use(pinia)
   // Refer to
   // https://github.com/antfu/vite-ssg/blob/main/README.md#state-serialization
