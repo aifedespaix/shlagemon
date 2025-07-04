791f40350870a65d9e59ea600a5ddaf8cfddddc7
Aife <clapette01@gmail.com>
2025-07-04 14:42:22 +0200

    remove audio module and start music on app mount

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { useAudioStore } from '~/stores/audio'
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
@@ -21,6 +22,13 @@ useHead({
       href: () => preferredDark.value ? '/favicon-dark.svg' : '/favicon.svg',
     },
   ],
+})
+
+const audio = useAudioStore()
+
+onMounted(() => {
+  if (audio.isMusicEnabled)
+    audio.playMusic('/audio/ambient.ogg')
 })
 </script>
 
