383498e35e4d648f69eb1f540a396e6078a9185f
Aife <clapette01@gmail.com>
2025-07-05 20:02:38 +0200

    feat: disable mobile menu buttons when dialog open

--- src/components/layout/MobileMenu.vue
+++ src/components/layout/MobileMenu.vue
@@ -1,11 +1,28 @@
 <script setup lang="ts">
+import { computed, watch } from 'vue'
 import SchlagedexIcon from '~/components/icons/schlagedex.vue'
 import Button from '~/components/ui/Button.vue'
+import { useAchievementsStore } from '~/stores/achievements'
+import { useDialogStore } from '~/stores/dialog'
+import { useInventoryStore } from '~/stores/inventory'
 import { useInventoryModalStore } from '~/stores/inventoryModal'
 import { useMobileTabStore } from '~/stores/mobileTab'
 
 const mobile = useMobileTabStore()
 const inventoryModal = useInventoryModalStore()
+const dialog = useDialogStore()
+const inventory = useInventoryStore()
+const achievements = useAchievementsStore()
+
+const menuDisabled = computed(() => dialog.isDialogVisible)
+const dexDisabled = menuDisabled
+const achievementsDisabled = computed(() => menuDisabled.value || !achievements.hasAny)
+const inventoryDisabled = computed(() => menuDisabled.value || inventory.list.length === 0)
+
+watch(dialog.isDialogVisible, (val) => {
+  if (val)
+    mobile.set('game')
+})
 
 function toggleInventory() {
   if (inventoryModal.isVisible)
@@ -21,6 +38,7 @@ function toggleInventory() {
       type="menu"
       class="aspect-square"
       :class="mobile.current === 'dex' ? 'text-teal-600 dark:text-teal-400' : ''"
+      :disabled="dexDisabled"
       @click="mobile.set('dex')"
     >
       <SchlagedexIcon class="h-5 w-5" />
@@ -29,6 +47,7 @@ function toggleInventory() {
       type="menu"
       class="aspect-square"
       :class="mobile.current === 'achievements' ? 'text-teal-600 dark:text-teal-400' : ''"
+      :disabled="achievementsDisabled"
       @click="mobile.set('achievements')"
     >
       <div class="i-carbon-trophy" />
@@ -45,6 +64,7 @@ function toggleInventory() {
       type="menu"
       class="aspect-square"
       :class="inventoryModal.isVisible ? 'text-teal-600 dark:text-teal-400' : ''"
+      :disabled="inventoryDisabled"
       @click="toggleInventory"
     >
       <div class="i-carbon-inventory-management" />
