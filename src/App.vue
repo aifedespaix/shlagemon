09503fd878714428052662db7797e6095e012560
Aife <clapette01@gmail.com>
2025-07-06 20:08:52 +0200

    add social meta tags

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,64 @@ useHead({
       content: 'Attrapes tous les Shlagémons pour éviter qu\'ils ne pourrissent la terre entière.',
     },
     {
+      name: 'keywords',
+      content: 'shlagemon, jeu, pokemon, parodie, capture, combat',
+    },
+    {
+      name: 'author',
+      content: 'Shlagémon Team',
+    },
+    {
       name: 'theme-color',
       content: () => isDark.value ? '#0d9488' : '#ffffff',
+    },
+    {
+      property: 'og:title',
+      content: 'Shlagémon - Ça sent très fort',
+    },
+    {
+      property: 'og:description',
+      content: 'Attrapes tous les Shlagémons pour éviter qu\'ils ne pourrissent la terre entière.',
+    },
+    {
+      property: 'og:type',
+      content: 'website',
+    },
+    {
+      property: 'og:url',
+      content: 'https://shlagemon.web.app',
+    },
+    {
+      property: 'og:image',
+      content: 'https://shlagemon.web.app/items/shlageball/shlageball.png',
+    },
+    {
+      property: 'og:site_name',
+      content: 'Shlagémon',
+    },
+    {
+      name: 'twitter:card',
+      content: 'summary_large_image',
+    },
+    {
+      name: 'twitter:title',
+      content: 'Shlagémon - Ça sent très fort',
+    },
+    {
+      name: 'twitter:description',
+      content: 'Attrapes tous les Shlagémons pour éviter qu\'ils ne pourrissent la terre entière.',
+    },
+    {
+      name: 'twitter:image',
+      content: 'https://shlagemon.web.app/items/shlageball/shlageball.png',
+    },
+    {
+      name: 'twitter:site',
+      content: '@shlagemon',
+    },
+    {
+      name: 'twitter:creator',
+      content: '@shlagemon',
     },
   ],
   link: [
@@ -19,6 +75,14 @@ useHead({
       rel: 'icon',
       type: 'image/svg+xml',
       href: () => preferredDark.value ? '/favicon-dark.svg' : '/favicon.svg',
+    },
+    {
+      rel: 'canonical',
+      href: 'https://shlagemon.web.app',
+    },
+    {
+      rel: 'image_src',
+      href: 'https://shlagemon.web.app/items/shlageball/shlageball.png',
     },
   ],
 })
