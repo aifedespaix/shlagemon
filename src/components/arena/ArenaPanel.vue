c1ed5fe2e6d40fa6bdfe7963133bd1b35668e2cb
Aife Portable <clapette01@gmail.com>
2025-07-07 18:20:11 +0200

    fix scroll arena

--- src/components/arena/ArenaPanel.vue
+++ src/components/arena/ArenaPanel.vue
@@ -120,91 +120,93 @@ onUnmounted(() => clearTimeout(nextTimer))
 </script>
 
 <template>
-  <div class="grid grid-cols-6 grid-rows-4 h-full w-full gap-2">
-    <div
-      v-for="enemy in enemyTeam"
-      :key="enemy.id"
-      class="border-red-600 rounded-full bg-red-500/40"
-    >
-      <ShlagemonImage :id="enemy.id" :alt="enemy.name" class="h-full w-full object-contain" />
-    </div>
-    <div v-for="enemy in enemyTeam" :key="enemy.id" class="flex-center flex-col gap-1 color-red-600">
-      <div class="i-game-icons:crossed-sabres text-2xl" />
-      <div class="text-sm">
-        VS
+  <div class="tiny-scrollbar overflow-auto">
+    <div class="grid grid-cols-6 grid-rows-4 h-full w-full gap-2">
+      <div
+        v-for="enemy in enemyTeam"
+        :key="enemy.id"
+        class="border-red-600 rounded-full bg-red-500/40"
+      >
+        <ShlagemonImage :id="enemy.id" :alt="enemy.name" class="h-full w-full object-contain" />
       </div>
-      <div class="i-carbon:chevron-down text-xl" />
-    </div>
+      <div v-for="enemy in enemyTeam" :key="enemy.id" class="flex-center flex-col gap-1 color-red-600">
+        <div class="i-game-icons:crossed-sabres text-2xl" />
+        <div class="text-sm">
+          VS
+        </div>
+        <div class="i-carbon:chevron-down text-xl" />
+      </div>
 
-    <button
-      v-for="(enemy, i) in enemyTeam"
-      :key="i"
-      class="border-blue-600 rounded-full bg-blue-500/30"
-      @click="openDex(i)"
-    >
-      <template v-if="playerSelection[i]">
-        <ShlagemonImage
-          :id="playerSelection[i]!.base.id"
-          :alt="playerSelection[i]!.base.name"
-          :shiny="playerSelection[i]!.isShiny"
-          class="h-full w-full object-contain"
-        />
-      </template>
-    </button>
+      <button
+        v-for="(enemy, i) in enemyTeam"
+        :key="i"
+        class="border-blue-600 rounded-full bg-blue-500/30"
+        @click="openDex(i)"
+      >
+        <template v-if="playerSelection[i]">
+          <ShlagemonImage
+            :id="playerSelection[i]!.base.id"
+            :alt="playerSelection[i]!.base.name"
+            :shiny="playerSelection[i]!.isShiny"
+            class="h-full w-full object-contain"
+          />
+        </template>
+      </button>
 
-    <div class="col-span-6 flex flex-col gap-2">
-      <Info v-if="playerSelection.some(m => !m)" color="danger" class="text-center text-xs">
-        Vous devez selectionner 6 Shlagémons pour combattre dans l'arène
-      </Info>
       <div class="col-span-6 flex flex-col gap-2">
-        <Info color="alert" class="text-center text-xs">
-          Le combat est automatique et se déroule sans clics.
+        <Info v-if="playerSelection.some(m => !m)" color="danger" class="text-center text-xs">
+          Vous devez selectionner 6 Shlagémons pour combattre dans l'arène
         </Info>
-        <Button
-          type="primary"
-          class="mx-auto"
-          :disabled="playerSelection.some(m => !m)"
-          @click="startBattle"
-        >
-          Combattre
-        </Button>
-      </div>
-      <Modal v-model="showDex" footer-close>
-        <h3 v-if="activeSlot !== null" class="mb-2 text-center text-lg font-bold">
-          Choisir un Shlagémon contre {{ enemyTeam[activeSlot].name }}
-        </h3>
-        <ShlagemonQuickSelect />
-      </Modal>
-
-      <Modal v-model="showDuel" :close-on-outside-click="false">
-        <ArenaDuel
-          v-if="showDuel && duelResult === null"
-          :player="arena.team[arena.currentIndex]"
-          :enemy="arena.enemyTeam[arena.currentIndex]"
-          @end="onDuelEnd"
-        />
-        <div v-else class="flex flex-col items-center gap-2">
-          <div
-            :class="duelResult === 'win'
-              ? 'text-green-600 font-bold dark:text-green-400'
-              : 'text-red-600 font-bold dark:text-red-400'"
+        <div class="col-span-6 flex flex-col gap-2">
+          <Info color="alert" class="text-center text-xs">
+            Le combat est automatique et se déroule sans clics.
+          </Info>
+          <Button
+            type="primary"
+            class="mx-auto"
+            :disabled="playerSelection.some(m => !m)"
+            @click="startBattle"
           >
-            {{ duelResult === 'win' ? 'Victoire !' : 'Défaite...' }}
-          </div>
-          <Button v-if="duelResult === 'win' && hasNextDuel" type="primary" @click="proceedNext">
-            Suivant
-          </Button>
-          <Button v-else-if="duelResult === 'win'" type="primary" @click="closeVictory">
-            Fermer
-          </Button>
-          <Button v-else type="danger" @click="closeAfterDefeat">
-            OK
+            Combattre
           </Button>
         </div>
-      </Modal>
-      <Modal v-model="showDefeat" :close-on-outside-click="false">
-        <ArenaDefeatDialog @retry="retryBattle" @quit="quitArena" />
-      </Modal>
+        <Modal v-model="showDex" footer-close>
+          <h3 v-if="activeSlot !== null" class="mb-2 text-center text-lg font-bold">
+            Choisir un Shlagémon contre {{ enemyTeam[activeSlot].name }}
+          </h3>
+          <ShlagemonQuickSelect />
+        </Modal>
+
+        <Modal v-model="showDuel" :close-on-outside-click="false">
+          <ArenaDuel
+            v-if="showDuel && duelResult === null"
+            :player="arena.team[arena.currentIndex]"
+            :enemy="arena.enemyTeam[arena.currentIndex]"
+            @end="onDuelEnd"
+          />
+          <div v-else class="flex flex-col items-center gap-2">
+            <div
+              :class="duelResult === 'win'
+                ? 'text-green-600 font-bold dark:text-green-400'
+                : 'text-red-600 font-bold dark:text-red-400'"
+            >
+              {{ duelResult === 'win' ? 'Victoire !' : 'Défaite...' }}
+            </div>
+            <Button v-if="duelResult === 'win' && hasNextDuel" type="primary" @click="proceedNext">
+              Suivant
+            </Button>
+            <Button v-else-if="duelResult === 'win'" type="primary" @click="closeVictory">
+              Fermer
+            </Button>
+            <Button v-else type="danger" @click="closeAfterDefeat">
+              OK
+            </Button>
+          </div>
+        </Modal>
+        <Modal v-model="showDefeat" :close-on-outside-click="false">
+          <ArenaDefeatDialog @retry="retryBattle" @quit="quitArena" />
+        </Modal>
+      </div>
     </div>
   </div>
 </template>
