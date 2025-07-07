43d4b32a4322b8d7a9c19efe171e0ebaec4a1629
Aife <clapette01@gmail.com>
2025-07-07 11:47:07 +0200

    feat(player): track arena badges and level cap

--- src/stores/player.ts
+++ src/stores/player.ts
@@ -6,6 +6,13 @@ export const usePlayerStore = defineStore('player', () => {
   const pseudo = ref('')
   const realName = ref('')
   const gender = ref<Character['gender']>('unknown')
+  const arenaBadges = ref<Record<string, boolean>>({})
+
+  const badgeCount = computed(() =>
+    Object.values(arenaBadges.value).filter(v => v).length,
+  )
+
+  const levelCap = computed(() => 10 + badgeCount.value * 10)
 
   const character = computed<Character>(() => ({
     id: 'player',
@@ -27,9 +34,29 @@ export const usePlayerStore = defineStore('player', () => {
     pseudo.value = ''
     realName.value = ''
     gender.value = 'unknown'
+    arenaBadges.value = {}
   }
 
-  return { pseudo, realName, gender, character, setPlayer, reset }
+  function earnBadge(id: string) {
+    arenaBadges.value[id] = true
+  }
+
+  function hasBadge(id: string) {
+    return !!arenaBadges.value[id]
+  }
+
+  return {
+    pseudo,
+    realName,
+    gender,
+    character,
+    arenaBadges,
+    levelCap,
+    setPlayer,
+    earnBadge,
+    hasBadge,
+    reset,
+  }
 }, {
   persist: true,
 })
