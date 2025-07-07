2b45ade2e23191b98baa747b6bb74a38e5ea344a
Aife Portable <clapette01@gmail.com>
2025-07-07 17:52:17 +0200

    fix shop

--- src/components/panels/ShopPanel.vue
+++ src/components/panels/ShopPanel.vue
@@ -19,29 +19,27 @@ function closeShop() {
 </script>
 
 <template>
-  <div class="h-full flex flex-col -m-4">
-    <h2 class="mb-2 text-center font-bold">
-      Boutique
-    </h2>
-    <div v-if="!selectedItem" class="flex flex-col gap-2 overflow-auto">
-      <ItemCard
-        v-for="item in shopItems"
-        :key="item.id"
-        :item="item"
-        class="cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-800"
-        @click="selectedItem = item"
-      >
-        <Button class="ml-auto text-xs" @click.stop="selectedItem = item">
-          Détails
-        </Button>
-      </ItemCard>
-    </div>
-    <div v-else class="flex-1 overflow-auto">
-      <ShopItemDetail :item="selectedItem" @close="selectedItem = null" />
-    </div>
-    <Button type="danger" class="mt-2 flex self-center gap-2" @click="closeShop">
-      <div class="i-carbon:exit" />
-      Quitter la boutique
-    </Button>
+  <h2 class="mb-2 text-center font-bold">
+    Boutique
+  </h2>
+  <div v-if="!selectedItem" class="flex flex-col gap-2 overflow-auto">
+    <ItemCard
+      v-for="item in shopItems"
+      :key="item.id"
+      :item="item"
+      class="cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-800"
+      @click="selectedItem = item"
+    >
+      <Button class="ml-auto text-xs" @click.stop="selectedItem = item">
+        Détails
+      </Button>
+    </ItemCard>
   </div>
+  <div v-else class="flex-1 overflow-auto">
+    <ShopItemDetail :item="selectedItem" @close="selectedItem = null" />
+  </div>
+  <Button type="danger" class="mt-2 flex self-center gap-2" @click="closeShop">
+    <div class="i-carbon:exit" />
+    Quitter la boutique
+  </Button>
 </template>
--- todo.md
+++ todo.md
@@ -1,17 +1,3 @@
-# pause combat quand dialog
-
-# parfois il y a plusieurs ocmbats en même temps
-
 # mettre de l'xp quand ona ttrape un shlaagémon
-
-si un duplicatas de shlagémon est capturé,
-on compare son niveau de rareté, si elle est supérieur à cette du shlagémon déjà possédé,
-la rareté passe à celle du nouveau shlagémon
-en contre partie, le shlagémon perd autant de niveau qu'il a gagné de points de rareté dans la limite du niveau 1 minimum,
-si le nouveau shlagémon à une rareté inférieure au notre, on monte quand même d'un niveau de rareté, et on lui retire 1 lvl
-on affiche égalmeent un toasdt explicant la rareté gagnée, les niveaux perdus.
-On garde quand même lme principe de, si le shlagémon est shiny, il transforme le notre en shiny si il l'est pas déjà.
-
-le niveau du shlagémon actuellement possédé
 
 # repenser la dafficulté de capture
