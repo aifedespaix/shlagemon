9cd37882f0cc1cafcff149d35fc6f8ee3ce6fbd3
Aife <clapette01@gmail.com>
2025-07-08 12:00:09 +0200

    feat: restrict capture by badge level

---
+++ src/components/battle/CaptureLimitModal.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Modal from '~/components/modal/Modal.vue'
+import { useCaptureLimitModalStore } from '~/stores/captureLimitModal'
+
+const modal = useCaptureLimitModalStore()
+</script>
+
+<template>
+  <Modal v-model="modal.isVisible" footer-close>
+    <div class="flex flex-col items-center gap-2">
+      <p class="text-center text-sm">
+        Pour pouvoir attraper ce Schlagémon de niveau {{ modal.requiredLevel }}, il vous faut un badge.
+        Rendez-vous dans le village pour défier la reine et remporter ce droit.
+      </p>
+    </div>
+  </Modal>
+</template>
