336ca8724e056f88d769c2587fbcb7ad6db2a85b
Aife <clapette01@gmail.com>
2025-07-09 16:15:50 +0200

    refactor(filters): compute sort direction

--- src/stores/inventoryFilter.ts
+++ src/stores/inventoryFilter.ts
@@ -1,24 +1,18 @@
 import { defineStore } from 'pinia'
-import { ref, watch } from 'vue'
+import { computed, ref } from 'vue'
+import { getSortDirection } from '~/utils/sort'
 
 export type InventorySort = 'name' | 'type' | 'price'
 
 export const useInventoryFilterStore = defineStore('inventoryFilter', () => {
   const search = ref('')
   const sortBy = ref<InventorySort>('name')
-  const sortAsc = ref(true)
-
-  watch(sortBy, (val) => {
-    if (val === 'price')
-      sortAsc.value = false
-    else
-      sortAsc.value = true
-  })
+  const sortAsc = computed(() =>
+    getSortDirection(sortBy.value, ['name', 'type']))
 
   function reset() {
     search.value = ''
     sortBy.value = 'name'
-    sortAsc.value = true
   }
 
   return { search, sortBy, sortAsc, reset }
