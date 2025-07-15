03f2085022f2d06c5b22a7d1efb7ff421ea1beaa
Aife Portable <clapette01@gmail.com>
2025-07-15 10:46:52 +0200

    string utils

---
+++ src/utils/string.ts
@@ -0,0 +1,11 @@
+export function safeStringify(obj: any, space = 2) {
+  const seen = new WeakSet()
+  return JSON.stringify(obj, (key, value) => {
+    if (typeof value === 'object' && value !== null) {
+      if (seen.has(value))
+        return '[Circular]'
+      seen.add(value)
+    }
+    return value
+  }, space)
+}
