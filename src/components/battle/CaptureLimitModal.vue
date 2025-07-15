dd077d5a4830745e9f60b004ecea4c29c3ca357b
Aife Portable <clapette01@gmail.com>
2025-07-15 18:52:41 +0200

    faute

--- src/components/battle/CaptureLimitModal.vue
+++ src/components/battle/CaptureLimitModal.vue
@@ -9,7 +9,7 @@ const modal = useCaptureLimitModalStore()
     <div class="flex flex-col items-center gap-2">
       <p class="text-center text-sm">
         Pour pouvoir attraper ce Shlagémon de niveau {{ modal.requiredLevel }}, il vous faut un badge.
-        Rendez-vous dans le village pour défier la reine et remporter ce droit.
+        Rendez-vous dans le village pour défier l'arène et remporter ce droit.
       </p>
     </div>
   </Modal>
