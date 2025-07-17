b29116fab9d30b83e1efb8391457836971910f69
Aife <clapette01@gmail.com>
2025-07-17 22:36:26 +0200

    feat: type shlagedex serializer

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,4 +1,4 @@
-export function safeStringify(obj: any, space = 2) {
+export function safeStringify<T>(obj: T, space = 2): string {
   const seen = new WeakSet()
   return JSON.stringify(obj, (key, value) => {
     if (typeof value === 'object' && value !== null) {
