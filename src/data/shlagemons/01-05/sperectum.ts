c3cc96a332fde08d07a7d639d7fae78a7a15ea8a
Aife Portable <clapette01@gmail.com>
2025-07-17 17:31:29 +0200

    mons 50-55

--- src/data/shlagemons/01-05/sperectum.ts
+++ src/data/shlagemons/01-05/sperectum.ts
@@ -1,12 +1,20 @@
 import type { BaseShlagemon } from '~/type'
 import { shlagemonTypes } from '../../shlagemons-type'
+import ectroudbal from '../evolutions/ectroudbal'
 
 export const sperectum: BaseShlagemon = {
   id: 'sperectum',
   name: 'Sperectum',
   description: `Sperectum est la réincarnation d'un pet émis par un Shlagemon.`,
   types: [shlagemonTypes.spectre],
-  coefficient: 5,
+  coefficient: 26,
+  evolution: {
+    base: ectroudbal,
+    condition: {
+      type: 'lvl',
+      value: 52,
+    },
+  },
 }
 
 export default sperectum
---
+++ src/data/shlagemons/50-55/tadsperm.ts
@@ -0,0 +1,24 @@
+import type { BaseShlagemon } from '~/type'
+import { shlagemonTypes } from '../../shlagemons-type'
+import grostadsperm from '../evolutions/grostadsperm'
+
+export const tadsperm: BaseShlagemon = {
+  id: 'tadsperm',
+  name: 'Tadsperm',
+  description: `Tadsperm est un tas visqueux au teint douteux, oscillant entre le gris laiteux et le beige maladif. Son corps dégouline de gouttes épaisses qui s’échappent en permanence, laissant derrière lui des traces suspectes et gluantes. Son sourire est béat, l’œil mi-clos, l’air totalement détaché du monde extérieur.
+
+Sa principale occupation : s’isoler dans des coins sombres pour savourer les plaisirs les plus solitaires de la vie. Son attaque signature, *Jet Spontané*, consiste à projeter une gerbe collante et opaque qui ralentit et colle l’adversaire sur place.
+
+On raconte qu’il passe plus de temps à “méditer” qu’à se battre. Certains dresseurs disent avoir glissé dessus dans les vestiaires…`,
+  types: [shlagemonTypes.poison],
+  coefficient: 52,
+  evolution: {
+    base: grostadsperm,
+    condition: {
+      type: 'lvl',
+      value: 59,
+    },
+  },
+}
+
+export default tadsperm
