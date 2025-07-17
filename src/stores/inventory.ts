8b1e6fcde254f4586e163c90879f40f55e89856a
Aife <clapette01@gmail.com>
2025-07-17 15:32:59 +0200

    Remove unused arena and trainer battle store references

--- src/stores/inventory.ts
+++ src/stores/inventory.ts
@@ -14,8 +14,6 @@ export const useInventoryStore = defineStore('inventory', () => {
   const items = ref<Record<string, number>>({})
   const game = useGameStore()
   const dex = useShlagedexStore()
-  const _arena = useArenaStore()
-  const _trainerBattle = useTrainerBattleStore()
   const featureLock = useFeatureLockStore()
   const player = usePlayerStore()
   const captureLimitModal = useCaptureLimitModalStore()
