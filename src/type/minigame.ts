c8972269e9840fe04c1682385e47cd160f11ed52
Aife <clapette01@gmail.com>
2025-07-19 00:55:30 +0200

    feat(minigame): reward grass egg

--- src/type/minigame.ts
+++ src/type/minigame.ts
@@ -1,13 +1,26 @@
 import type { Component } from 'vue'
 import type { Character } from './character'
 import type { DialogNode } from './dialog'
+import type { ItemId } from '~/data/items/items'
+
+export interface MoneyReward {
+  type: 'money'
+  amount: number
+}
+
+export interface ItemReward {
+  type: 'item'
+  itemId: ItemId
+}
+
+export type MiniGameReward = MoneyReward | ItemReward
 
 export interface MiniGameDefinition {
   id: MiniGameId
   label: string
   character: Character
   component: () => Promise<{ default: Component }>
-  reward: number
+  reward: MiniGameReward
   createIntro: (start: () => void) => DialogNode[]
   createSuccess: (done: () => void) => DialogNode[]
   createFailure: (done: () => void) => DialogNode[]
