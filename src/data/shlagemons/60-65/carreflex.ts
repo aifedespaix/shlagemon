0fb4c89cea452c8c8a72dfefd7daa031278f0f22
Aife <clapette01@gmail.com>
2025-07-19 10:58:50 +0200

    feat(i18n): merge component locales

--- src/data/shlagemons/60-65/carreflex.ts
+++ src/data/shlagemons/60-65/carreflex.ts
@@ -1,10 +1,12 @@
 import type { BaseShlagemon } from '~/type'
 import { shlagemonTypes } from '../../shlagemons-type'
 
+export const CARREFLEX_DESCRIPTION = 'carreflex.description'
+
 export const carreflex: BaseShlagemon = {
   id: 'carreflex',
   name: 'Carréflex',
-  description: `Carréflex est un colosse tout en angles qui dort généralement en plein milieu de la route. Sa forme cubique lui permet de bloquer même les camions.`,
+  description: CARREFLEX_DESCRIPTION,
   types: [shlagemonTypes.normal],
   coefficient: 62,
 }
--- src/data/shlagemons/75-80/dentlait.ts
+++ src/data/shlagemons/75-80/dentlait.ts
@@ -2,10 +2,12 @@ import type { BaseShlagemon } from '~/type'
 import { shlagemonTypes } from '../../shlagemons-type'
 import hosoltueur from '../evolutions/hosoltueur'
 
+export const DENTLAIT_DESCRIPTION = 'dentlait.description'
+
 export const dentlait: BaseShlagemon = {
   id: 'dentlait',
   name: 'Dentlait',
-  description: `Dentlait n'a qu'une seule dent qui menace de tomber à tout moment. Il la protège jalousement et mord tout ce qui passe pour prouver qu'elle tient encore en place.`,
+  description: DENTLAIT_DESCRIPTION,
   types: [shlagemonTypes.sol],
   coefficient: 77,
   evolution: {
