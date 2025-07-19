f0252b8bdb059d21058860efe566ea1e9cf1c091
Aife <clapette01@gmail.com>
2025-07-19 09:16:51 +0200

    feat: add wearable item icon component

---
+++ src/components/inventory/WearableItemIcon.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type { Item } from '~/type/item'
+
+const props = defineProps<{ item: Item }>()
+</script>
+
+<template>
+  <div
+    v-if="props.item.icon"
+    v-bind="$attrs"
+    :title="props.item.name"
+    :class="[props.item.icon, props.item.iconClass]"
+  />
+  <img
+    v-else-if="props.item.image"
+    v-bind="$attrs"
+    :title="props.item.name"
+    :src="props.item.image"
+    :alt="props.item.name"
+    class="object-contain"
+  >
+</template>
