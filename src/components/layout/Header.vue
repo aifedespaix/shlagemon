c4fdac9be4e8a0b374c7b9fac5422d3249176997
Aife Portable <clapette01@gmail.com>
2025-07-20 00:48:30 +0200

    fix url

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -30,11 +30,11 @@ function onDoubleClick() {
 <template>
   <header class="h-12 flex items-center justify-between bg-gray-100 p-4 dark:bg-gray-800">
     <div class="flex items-center gap-2">
-      <img src="/logo.png" :alt="t('header.logoAlt')" class="h-20 -my-4">
+      <img src="/logo.png" :alt="t('components.layout.Header.logoAlt')" class="h-20 -my-4">
       <div class="flex flex-col text-xs leading-tight">
-        <span class="font-bold sm:text-sm">{{ t('header.title') }}</span>
-        <span>{{ t('header.language') }}: {{ locale }}</span>
-        <span>{{ t('header.locale') }}: {{ currentLocale }}</span>
+        <span class="font-bold sm:text-sm">{{ t('components.layout.Header.title') }}</span>
+        <span>{{ t('components.layout.Header.language') }}: {{ locale }}</span>
+        <span>{{ t('components.layout.Header.locale') }}: {{ currentLocale }}</span>
       </div>
     </div>
     <div class="flex items-center gap-2">
@@ -42,21 +42,21 @@ function onDoubleClick() {
       <ThemeToggle />
       <UiButton
         type="icon"
-        :aria-label="t('header.audio')"
+        :aria-label="t('components.layout.Header.audio')"
         @click.stop="onClick"
         @dblclick.stop="onDoubleClick"
       >
         <div :class="audio.isMusicEnabled ? 'i-carbon-volume-up' : 'i-carbon-volume-mute'" />
       </UiButton>
       <AudioSettingsModal v-model="showAudio" />
-      <UiButton type="icon" :aria-label="t('header.settings')" @click="showSettings = true">
+      <UiButton type="icon" :aria-label="t('components.layout.Header.settings')" @click="showSettings = true">
         <div class="i-carbon-settings" />
       </UiButton>
       <SettingsSettingsModal v-model="showSettings" />
       <UiButton
         v-if="showDevButton"
         type="icon"
-        :aria-label="t('header.developer')"
+        :aria-label="t('components.layout.Header.developer')"
         @click="showDeveloper = true"
       >
         <div class="i-carbon-debug" />
