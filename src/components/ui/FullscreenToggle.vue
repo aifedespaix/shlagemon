d67467fbbfe0f04ad4ce113c709023435d81833a
Aife <clapette01@gmail.com>
2025-07-19 18:57:03 +0200

    feat(layout): add language info and fullscreen toggle

---
+++ src/components/ui/FullscreenToggle.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const isFullscreen = ref(false)
+
+function toggle() {
+  if (!isFullscreen.value) {
+    document.documentElement.requestFullscreen?.()
+  }
+  else {
+    document.exitFullscreen?.()
+  }
+}
+
+useEventListener(document, 'fullscreenchange', () => {
+  isFullscreen.value = !!document.fullscreenElement
+})
+</script>
+
+<template>
+  <UiButton type="icon" aria-label="Plein écran" @click="toggle">
+    <div :class="isFullscreen ? 'i-carbon-minimize' : 'i-carbon-maximize'" />
+  </UiButton>
+</template>
