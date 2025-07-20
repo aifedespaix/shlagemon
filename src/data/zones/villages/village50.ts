583a6e510fecaaacbaaca6a0977883a41f825499
Aife <clapette01@gmail.com>
2025-07-20 16:48:48 +0200

    refactor: split villages data files and update shops

---
+++ src/data/zones/villages/village50.ts
@@ -0,0 +1,51 @@
+import type { Zone } from '~/type'
+import {
+  attackPotion,
+  capturePotion,
+  defensePotion,
+  hyperCapturePotion,
+  potion,
+  steroids,
+  superAttackPotion,
+  superCapturePotion,
+  superDefensePotion,
+  superPotion,
+  superVitalityPotion,
+  superXpPotion,
+  vitalityPotion,
+  xpPotion,
+} from '~/data/items/items'
+import { shlageball, superShlageball } from '~/data/items/shlageball'
+
+export const village50: Zone = {
+  id: 'village-caca-boudin',
+  name: 'Village du Caca Boudin',
+  type: 'village',
+  actions: [],
+  minLevel: 50,
+  village: {
+    shop: {
+      items: [
+        potion,
+        superPotion,
+        defensePotion,
+        superDefensePotion,
+        attackPotion,
+        superAttackPotion,
+        vitalityPotion,
+        superVitalityPotion,
+        xpPotion,
+        superXpPotion,
+        capturePotion,
+        superCapturePotion,
+        shlageball,
+        superShlageball,
+        hyperCapturePotion,
+        steroids,
+      ],
+    },
+    poulailler: {
+      icon: 'i-game-icons:bird-house',
+    },
+  },
+}
