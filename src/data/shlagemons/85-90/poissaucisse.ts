77606a796548318b45423fccfb98caf427ba53d2
Aife Portable <clapette01@gmail.com>
2025-07-23 19:50:25 +0200

    mass mons

--- src/data/shlagemons/85-90/poissaucisse.ts
+++ src/data/shlagemons/85-90/poissaucisse.ts
@@ -2,11 +2,11 @@ import type { BaseShlagemon } from '~/type'
 import { shlagemonTypes } from '../../shlagemons-type'
 import poissomerguez from '../evolutions/poissomerguez'
 
-export const poissocisse: BaseShlagemon = {
-  id: 'poissocisse',
-  name: 'Poissocisse',
+export const poissaucisse: BaseShlagemon = {
+  id: 'poissaucisse',
+  name: 'Poissaucisse',
   description: `Poissocisse se nourrit exclusivement de saucisses. Son odeur charcutière attire chiens et dresseurs affamés, ce qui le rend paradoxalement difficile à pêcher.`,
-  descriptionKey: 'data.shlagemons.85-90.poissocisse.description',
+  descriptionKey: 'data.shlagemons.85-90.poissaucisse.description',
   types: [shlagemonTypes.eau],
   coefficient: 88,
   evolution: {
@@ -15,4 +15,4 @@ export const poissocisse: BaseShlagemon = {
   },
 }
 
-export default poissocisse
+export default poissaucisse
