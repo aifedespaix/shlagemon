2faede0608a3835eacce86658851b66d7a452afb
Aife Portable <clapette01@gmail.com>
2025-07-26 17:11:54 +0200

    exit poulailler

--- src/components/layout/TitledPanel.vue
+++ src/components/layout/TitledPanel.vue
@@ -11,12 +11,10 @@ const emit = defineEmits(['exit'])
     <div class="flex flex-1 flex-col gap-2 overflow-hidden">
       <slot />
     </div>
-    <div class="flex flex-wrap justify-end gap-2 bg-white dark:bg-gray-900">
+    <div class="flex flex-wrap justify-end gap-2 bg-white p-1 dark:bg-gray-900">
       <slot name="footer">
         <UiButton
-          type="danger"
-          variant="outline"
-          class="w-full flex gap-2 text-xs md:w-auto md:flex-col"
+          type="danger" variant="outline" class="flex gap-2 text-xs"
           @click="emit('exit')"
         >
           <div class="i-carbon:exit" />
