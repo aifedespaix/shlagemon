70382e4816149fb13ce20e76bc9f6c62d3f9aae8
Aife <clapette01@gmail.com>
2025-07-26 17:58:35 +0200

    feat: wrap village and mini-games in TitledPanel

--- src/components/panel/Village.vue
+++ src/components/panel/Village.vue
@@ -2,19 +2,30 @@
 import ZoneActions from '../village/ZoneActions.vue'
 
 const zone = useZoneStore()
+const panel = useMainPanelStore()
+const { t } = useI18n()
+
+function leaveVillage() {
+  if (zone.current.attachedTo)
+    zone.setZone(zone.current.attachedTo)
+  panel.showBattle()
+}
 </script>
 
 <template>
-  <div class="flex flex-col items-center gap-2">
-    <UiImageByBackground
-      v-if="zone.current.image"
-      :src="zone.current.image"
-      :alt="zone.current.name"
-      class="aspect-video h-full max-h-60 w-full object-contain md:max-h-80"
-    />
-    <h2 class="text-center font-bold">
-      {{ zone.current.name }}
-    </h2>
-    <ZoneActions />
-  </div>
+  <LayoutTitledPanel
+    :title="zone.current.name"
+    :exit-text="t('components.panel.Village.exit')"
+    @exit="leaveVillage"
+  >
+    <div class="flex flex-col items-center gap-2">
+      <UiImageByBackground
+        v-if="zone.current.image"
+        :src="zone.current.image"
+        :alt="zone.current.name"
+        class="aspect-video h-full max-h-60 w-full object-contain md:max-h-80"
+      />
+      <ZoneActions />
+    </div>
+  </LayoutTitledPanel>
 </template>
