ff751a34ce1b562466280be9eac2a6ba17b7aec7
Aife <clapette01@gmail.com>
2025-07-26 12:30:37 +0200

    fix store circular dependency

--- src/stores/zoneVisit.ts
+++ src/stores/zoneVisit.ts
@@ -1,7 +1,8 @@
 import { defineStore } from 'pinia'
+import { zonesData } from '~/data/zones'
 
 export const useZoneVisitStore = defineStore('zoneVisit', () => {
-  const visited = ref<Record<string, boolean>>({})
+  const visited = ref<Record<string, boolean>>({ [zonesData[0].id]: true })
   const dex = useShlagedexStore()
 
   const { accessibleZones } = useZoneAccess(toRef(dex, 'highestLevel'))
