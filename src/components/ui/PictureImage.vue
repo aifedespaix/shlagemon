8928d7f120ad1841cdd589fbcc8a032f9f06ea34
Aife <clapette01@gmail.com>
2025-07-27 12:30:22 +0200

    feat(ui): add PictureImage component

---
+++ src/components/ui/PictureImage.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const props = defineProps<{
+  src: string
+  alt?: string
+  sizes?: string
+}>()
+
+const webpSrc = computed(() => props.src.replace(/\.[a-z]+$/i, '.webp'))
+</script>
+
+<template>
+  <picture>
+    <source :srcset="webpSrc" type="image/webp" :sizes="props.sizes">
+    <img :src="props.src" :alt="props.alt" :sizes="props.sizes" v-bind="$attrs">
+  </picture>
+</template>
