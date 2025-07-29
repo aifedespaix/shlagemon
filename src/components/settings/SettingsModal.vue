15719c96b4665aea1a4994a0deffd1afc8db8e06
Aife <clapette01@gmail.com>
2025-07-29 20:00:33 +0200

    feat(settings): hide shortcuts tab on mobile

--- src/components/settings/SettingsModal.vue
+++ src/components/settings/SettingsModal.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { storeToRefs } from 'pinia'
 import LanguageTab from './LanguageTab.vue'
 import SaveTab from './SaveTab.vue'
 import SettingsShortcutsTab from './ShortcutsTab.vue'
@@ -17,24 +18,37 @@ function removeSave() {
   close()
 }
 
-const tabs = [
-  {
-    label: { text: 'Sauvegarde', icon: 'i-carbon-save' },
-    component: defineComponent({
-      name: 'SettingsSaveTabWrapper',
-      setup() {
-        return () => h(SaveTab, { onRemove: removeSave })
-      },
-    }),
-  },
-  {
-    label: { text: 'Raccourcis', icon: 'i-carbon-keyboard' },
-    component: SettingsShortcutsTab,
-  },
-  { label: { text: 'Langue', icon: 'i-carbon-language' }, component: LanguageTab },
-] as const
+const { isMobile } = storeToRefs(useUIStore())
+
+const tabs = computed(() => {
+  const arr = [
+    {
+      label: { text: 'Sauvegarde', icon: 'i-carbon-save' },
+      component: defineComponent({
+        name: 'SettingsSaveTabWrapper',
+        setup() {
+          return () => h(SaveTab, { onRemove: removeSave })
+        },
+      }),
+    },
+    { label: { text: 'Langue', icon: 'i-carbon-language' }, component: LanguageTab },
+  ]
+
+  if (!isMobile.value) {
+    arr.splice(1, 0, {
+      label: { text: 'Raccourcis', icon: 'i-carbon-keyboard' },
+      component: SettingsShortcutsTab,
+    })
+  }
+
+  return arr
+})
 
 const activeTab = ref(0)
+watch(tabs, (val) => {
+  if (activeTab.value >= val.length)
+    activeTab.value = val.length - 1
+})
 </script>
 
 <template>
