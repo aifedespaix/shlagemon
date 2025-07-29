99e3784a4073bb98f51ae6c7c9b9325d1f656e2e
Aife <clapette01@gmail.com>
2025-07-29 19:13:57 +0200

    feat(i18n): translate panel grid and battle panels

--- src/components/battle/CaptureLimitModal.vue
+++ src/components/battle/CaptureLimitModal.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 const modal = useCaptureLimitModalStore()
+const { t } = useI18n()
 </script>
 
 <template>
   <UiModal v-model="modal.isVisible" footer-close>
     <div class="flex flex-col items-center gap-2">
       <p class="text-center text-sm">
-        Pour pouvoir attraper ce Shlagémon de niveau {{ modal.requiredLevel }}, il vous faut un badge.
-        Rendez-vous dans le village pour défier l'arène et remporter ce droit.
+        {{ t('components.battle.CaptureLimitModal.text', { level: modal.requiredLevel }) }}
       </p>
     </div>
   </UiModal>
