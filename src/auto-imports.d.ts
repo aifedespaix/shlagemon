ed3ec7990ebb6718654c66674b58af1df0cc75ea
Aife <clapette01@gmail.com>
2025-08-02 10:54:57 +0200

    feat: centralize page metadata

--- src/auto-imports.d.ts
+++ src/auto-imports.d.ts
@@ -283,6 +283,7 @@ declare global {
   const useOdorElixirStore: typeof import('./stores/odorElixir')['useOdorElixirStore']
   const useOffsetPagination: typeof import('@vueuse/core')['useOffsetPagination']
   const useOnline: typeof import('@vueuse/core')['useOnline']
+  const usePageHead: typeof import('./composables/usePageHead')['default']
   const usePageLeave: typeof import('@vueuse/core')['usePageLeave']
   const useParallax: typeof import('@vueuse/core')['useParallax']
   const useParentElement: typeof import('@vueuse/core')['useParentElement']
@@ -412,6 +413,9 @@ declare global {
   // @ts-ignore
   export type { Cell } from './composables/useBattleship'
   import('./composables/useBattleship')
+  // @ts-ignore
+  export type { PageHeadOptions } from './composables/usePageHead'
+  import('./composables/usePageHead')
   // @ts-ignore
   export type { SlidingPuzzle, PuzzleDirection } from './composables/useSlidingPuzzle'
   import('./composables/useSlidingPuzzle')
@@ -739,6 +743,7 @@ declare module 'vue' {
     readonly useOdorElixirStore: UnwrapRef<typeof import('./stores/odorElixir')['useOdorElixirStore']>
     readonly useOffsetPagination: UnwrapRef<typeof import('@vueuse/core')['useOffsetPagination']>
     readonly useOnline: UnwrapRef<typeof import('@vueuse/core')['useOnline']>
+    readonly usePageHead: UnwrapRef<typeof import('./composables/usePageHead')['default']>
     readonly usePageLeave: UnwrapRef<typeof import('@vueuse/core')['usePageLeave']>
     readonly useParallax: UnwrapRef<typeof import('@vueuse/core')['useParallax']>
     readonly useParentElement: UnwrapRef<typeof import('@vueuse/core')['useParentElement']>
