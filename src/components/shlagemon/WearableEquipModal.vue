4d0f7fc7226c65308da7a5ea23e9a425e757bd44
Aife <clapette01@gmail.com>
2025-08-04 11:36:47 +0200

    refactor: use UiListItem in wearable equip modal

--- src/components/shlagemon/WearableEquipModal.vue
+++ src/components/shlagemon/WearableEquipModal.vue
@@ -9,21 +9,26 @@ const { t } = useI18n()
       <h3 class="text-center text-lg font-bold">
         {{ t('components.shlagemon.WearableEquipModal.title') }}
       </h3>
-      <div v-if="store.options.length" class="w-full flex flex-col gap-2">
-        <UiButton
+      <div v-if="store.options.length" class="w-full flex flex-col gap-2 p-1">
+        <UiListItem
           v-for="o in store.options"
           :key="o.item.id"
-          class="flex items-center justify-between gap-2"
+          as="button"
+          class="items-center justify-between"
           :disabled="o.qty <= 0"
           @click="store.equip(o.item.id)"
         >
-          <div class="flex items-center gap-2">
-            <div v-if="o.item.icon" class="h-6 w-6" :class="[o.item.icon, o.item.iconClass]" />
-            <img v-else-if="o.item.image" :src="o.item.image" :alt="t(o.item.name)" class="h-6 w-6 object-contain">
-            <span>{{ t(o.item.name) }}</span>
-          </div>
-          <span class="text-xs font-bold">x{{ o.qty }}</span>
-        </UiButton>
+          <template #left>
+            <div class="flex items-center gap-2">
+              <div v-if="o.item.icon" class="h-6 w-6" :class="[o.item.icon, o.item.iconClass]" />
+              <img v-else-if="o.item.image" :src="o.item.image" :alt="t(o.item.name)" class="h-6 w-6 object-contain">
+              <span>{{ t(o.item.name) }}</span>
+            </div>
+          </template>
+          <template #right>
+            <span class="text-xs font-bold">x{{ o.qty }}</span>
+          </template>
+        </UiListItem>
       </div>
       <p v-else class="text-center text-sm">
         {{ t('components.shlagemon.WearableEquipModal.noAvailable') }}
