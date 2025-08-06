b7f4bc08c66bdfc2124d14430627c262543bfa4c
Aife <clapette01@gmail.com>
2025-08-06 11:47:50 +0200

    chore: add type chart translations

--- src/components/shlagemon/TypeChartModal.vue
+++ src/components/shlagemon/TypeChartModal.vue
@@ -1,16 +1,17 @@
 <script setup lang="ts">
 import TypeChart from './TypeChart.vue'
 
+const { t } = useI18n()
 const modal = useTypeChartModalStore()
 </script>
 
 <template>
   <UiModal v-model="modal.isVisible" footer-close>
-    <UiPanelWrapper title="Table des types">
+    <UiPanelWrapper :title="t('components.shlagemon.TypeChartModal.title')">
       <template #icon>
         <div class="i-carbon-data-table" />
       </template>
-      <TypeChart :highlight="modal.highlight" />
+      <TypeChart />
     </UiPanelWrapper>
   </UiModal>
 </template>
