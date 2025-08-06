70f3c2c980fe146a8fa2703541ef7e4ef03ab8b1
Aife <clapette01@gmail.com>
2025-08-06 10:53:04 +0200

    feat(settings): display tab titles

--- src/components/settings/InterfaceTab.vue
+++ src/components/settings/InterfaceTab.vue
@@ -8,6 +8,9 @@ const { showVillagesOnMap } = storeToRefs(interfaceStore)
 
 <template>
   <div class="flex flex-col gap-4">
+    <h3 class="text-center text-lg font-bold">
+      {{ t('components.settings.SettingsModal.tabs.interface') }}
+    </h3>
     <UiCheckBox
       v-model="showVillagesOnMap"
       :label="t('components.settings.InterfaceTab.villagesOnMap')"
