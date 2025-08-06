390c576e629e342d7b1335ce3b81260741422830
Aife Portable <clapette01@gmail.com>
2025-08-06 11:50:57 +0200

    u

--- src/components/shlagemon/TypeChartModal.vue
+++ src/components/shlagemon/TypeChartModal.vue
@@ -9,7 +9,7 @@ const modal = useTypeChartModalStore()
   <UiModal v-model="modal.isVisible">
       <h3 class="text-lg font-bold flex items-center gap-2">
         <div class="i-carbon-data-table" />
-        <div>t('components.shlagemon.TypeChartModal.title')</div>
+        <div>{{t('components.shlagemon.TypeChartModal.title')}}</div>
       </h3>
       <TypeChart :highlight="modal.highlight" />
   </UiModal>
