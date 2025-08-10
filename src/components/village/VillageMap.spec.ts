00f3fa482b46296c10f9e957c979e6c8b87dc482
Aife <clapette01@gmail.com>
2025-08-10 23:15:40 +0200

    test: improve VillageMap spec typing

--- src/components/village/VillageMap.spec.ts
+++ src/components/village/VillageMap.spec.ts
@@ -1,10 +1,10 @@
-import type { VillageZone } from '~/type'
+import type { VillagePOI, VillageZone, VillageZoneId } from '~/type'
 import { mount } from '@vue/test-utils'
 import { describe, expect, it } from 'vitest'
-import VillageMap from './VillageMap.vue'
+import VillageMap from './Map.vue'
 
 const baseVillage: VillageZone = {
-  id: 'test-village',
+  id: 'village-paume' as VillageZoneId,
   name: 'Test',
   type: 'village',
   villageType: 'basic',
@@ -37,15 +37,15 @@ describe('village map', () => {
     const wrapper = mount(VillageMap, { props: { village: baseVillage } })
     const marker = wrapper.element.querySelector('.leaflet-marker-icon') as HTMLElement
     marker.dispatchEvent(new Event('click'))
-    const events = wrapper.emitted('select')
+    const events = wrapper.emitted('select') as Array<[VillagePOI]> | undefined
     expect(events).toBeTruthy()
     expect(events![0][0].id).toBe('shop')
   })
 
   it('updates markers when village changes', async () => {
     const wrapper = mount(VillageMap, { props: { village: baseVillage } })
-    const newVillage: VillageZone = { ...baseVillage, id: 'v2', pois: {} }
-    await wrapper.setProps({ village: newVillage })
+    const newVillage: VillageZone = { ...baseVillage, id: 'village-boule' as VillageZoneId, pois: {} }
+    await wrapper.setProps({ village: newVillage } as Partial<InstanceType<typeof VillageMap>['$props']>)
     const markers = wrapper.element.querySelectorAll('.leaflet-marker-icon')
     expect(markers.length).toBe(0)
   })
